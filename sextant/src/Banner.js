221f33451c5b13bea87b65284e842e594dd226a1
Mac <mac@Ronald.local>
2024-03-25 04:53:26 +1100

    Complete task 2

---
+++ sextant/src/Banner.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./Banner.css";
+
+class Banner extends Component {
+    render() {
+        return (
+            <div className="Banner">
+                <h1>Sextant</h1>
+            </div>
+        );
+    }
+}
+
+export default Banner;
